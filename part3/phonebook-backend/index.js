af380a7ed891d32987c625aee51d3ebaa03af31a
Hgrpoiu <hgrpoiu@gmail.com>
2022-08-14 15:18:36 -0500

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,6 +6,7 @@ const today = new Date();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 morgan.token("sent", (req, res) => {
   return JSON.stringify(req.body);
 });
