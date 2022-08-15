fe184852707b0e8106f643ef2840f5411a0f0166
Hgrpoiu <hgrpoiu@gmail.com>
2022-08-14 19:54:41 -0500

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -24,7 +24,7 @@ app.get("/api/persons", (reqeust, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
+  const id = request.params.id;
   Person.findById(id).then((p) => {
     response.json(p);
   });
