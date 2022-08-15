1a3f3720b5d4062cf69cb599a7c55bd1c960d94b
Hgrpoiu <hgrpoiu@gmail.com>
2022-08-14 23:36:56 -0500

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,6 +6,18 @@ const Person = require("./models/person");
 const app = express();
 const today = new Date();
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformed id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({ error: error.message });
+  }
+
+  next(error);
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
@@ -17,61 +29,87 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :sent")
 );
 
-app.get("/api/persons", (reqeust, response) => {
-  Person.find({}).then((p) => {
-    response.json(p);
-  });
+app.use(errorHandler);
+
+app.get("/api/persons", (reqeust, response, next) => {
+  Person.find({})
+    .then((p) => {
+      response.json(p);
+    })
+    .catch((err) => next(err));
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  Person.findById(id).then((p) => {
-    response.json(p);
-  });
+  Person.findById(id)
+    .then((p) => {
+      response.json(p);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<h1>there are ${people.length} entries</h1><div>As of ${
-      today.getMonth() + 1
-    }-${today.getDate()}-${today.getFullYear()} </div>`
-  );
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  people = people.filter((p) => {
-    return p.id !== id;
+  Person.find({}).then((result) => {
+    response.send(
+      `<h1>there are ${result.length} entries</h1><div>As of ${
+        today.getMonth() + 1
+      }-${today.getDate()}-${today.getFullYear()} </div>`
+    );
   });
-
-  response.status(204).end();
 });
 
 app.post("/api/persons", (request, response) => {
   const person = request.body;
+  const p = new Person({
+    name: person.name,
+    number: person.number,
+  });
   //Error handling
   if (!person.name || !person.number) {
     return response.status(400).json({
       error: "missing content",
     });
   }
-  if (
-    people.some((p) => {
-      return p.name === person.name;
+
+  //You should return the file which was added (I don't know why)
+  p.save().then((savedP) => {
+    response.json(savedP);
+  });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updated) => {
+      response.json(updated);
     })
-  ) {
-    return response.status(400).json({
-      error: "name is already present",
-    });
-  }
-
-  newPerson = {
-    name: person.name,
-    number: person.number,
-  };
-
-  people = people.concat(newPerson);
-  response.json(newPerson);
+    .catch((err) => next(err));
+  //MySolution?
+  // new Person({
+  //   name: request.body.name,
+  //   number: request.body.number,
+  //   _id: request.body.id,
+  // })
+  //   .save()
+  //   .then((savedP) => {
+  //     response.json(savedP);
+  //   });
 });
 
 const PORT = process.env.PORT;
